44e3b41d62abab3dd91eb5d0f1442dda1f72e942
devolik13 <devolik13@gmail.com>
2025-08-11 16:43:41 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,3 +3,5 @@ const SUPABASE_URL = 'https://vuzrbgtczuprgnysdbyz.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1enJiZ3RjenVwcmdueXNkYnl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3MzU0NzQsImV4cCI6MjA3MDMxMTQ3NH0.OunYZc1E26onsgCI8yNdpr-DOEcASxDGbFmvlfjFgCc';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export { supabase };
